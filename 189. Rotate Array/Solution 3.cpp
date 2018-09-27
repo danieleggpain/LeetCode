8acd8c5d680a8a3d7c01a71776bb52d19aa0fa33
Daniel Yan <z47yan@edu.uwaterloo.ca>
2018-09-27 02:49:53 -0400

    Update Solution 3.cpp

--- 189. Rotate Array/Solution 3.cpp
+++ 189. Rotate Array/Solution 3.cpp
@@ -1,3 +1,9 @@
+//Solution 3 got rid of recursion. The most important line in this solution is line 9, 
+//  where I realized that k can always be modified such that k is smaller than the size of the array. 
+//  For example, for an array [1, 2, 3, 4, 5], rotating 5 times is the same as not rotating at all. 
+//  Rotating 6 times is the same as rotating 1 time. 
+//This algorithm beats 100.00% of cpp submissions.
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
